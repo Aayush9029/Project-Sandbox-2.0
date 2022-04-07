dde80cc718a16ab7ed98737ed5b9158a90ac7c90
Aayush9029 <aayushpokharel36@gmail.com>
2022-04-07 12:30:51 -0400

    Initial Commit

---
+++ Project Sandbox 2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project Sandbox 2.0
+//
+//  Created by Aayush Pokharel on 2022-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project Sandbox 2.0/Project_Sandbox_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_Sandbox_2_0App.swift
+//  Project Sandbox 2.0
+//
+//  Created by Aayush Pokharel on 2022-04-07.
+//
+
+import SwiftUI
+
+@main
+struct Project_Sandbox_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
